e71622bc7a3e447dd1332d5aab0ecbac1c024629
Franziskus Nakajima <franziskus.nakajima@gmail.com>
2016-11-11 21:01:03 +0100

    refactor: Remove practice code, start final project

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,64 +1,14 @@
-import sys
-from sqlalchemy import Column, \
-                       ForeignKey, \
-                       Integer, \
-                       String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-# Create base class
 Base = declarative_base()
 
-# Create Restaurant class
-class Restaurant(Base):
-    '''
-    Database for restaurants
+class User(Base):
+    __tablename__ = "user"
+    
 
-    Restaurants in the restaurant table have an ID, a name and a cuisine.
-    '''
-    __tablename__ = 'restaurant'
+engine = create_engine('sqlite:///pmoi.db')
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-
-    @property
-    def serialize(self):
-        return {
-            'name':self.name
-        }
-
-
-# Create MenuItem class:
-class MenuItem(Base):
-    '''
-    Database for menu items
-
-    Menu items in the menu_item table have an ID, a name, a course it belongs
-    to, a description, a price and a foreign key restaurant_id.
-    '''
-    __tablename__ = 'menu_item'
-
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    course = Column(String(250))
-    description = Column(String(250))
-    price = Column(String(8))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-
-    @property
-    def serialize(self):
-        # Returns object in eaily serialisable format
-        return {
-            'name': self.name,
-            'description': self.description,
-            'price': self.price,
-            'course': self.course,
-        }
-
-
-
-### Keep this at the end of the file ###
-engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
---
+++ vagrant/catalog/project.py
@@ -0,0 +1,7 @@
+from flask import Flask, \
+                  request, \
+                  render_template, \
+                  redirect, \
+                  url_for, \
+                  jsonify, \
+                  flash
